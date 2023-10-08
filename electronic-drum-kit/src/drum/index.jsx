0193cb7dfb513efe421d73591c43bef1113d973a
sushant babu prasai <sushantprasaii7@gmail.com>
2023-10-08 21:43:19 +0545

    project setup

--- electronic-drum-kit/src/drum/index.jsx
+++ electronic-drum-kit/src/drum/index.jsx
@@ -2,11 +2,11 @@ import {  StyledKit, Container } from "./styling"
 
 export default function DrumKit() {
   return (
-     <Container className="grid grid-rows-2">
+     <Container className="">
       <div className="grid grid-cols-3">
-      <StyledKit className="">clap</StyledKit>
-      <StyledKit>hihit</StyledKit>
+      <StyledKit className="">crash</StyledKit>
       <StyledKit>kick</StyledKit>
+      <StyledKit>ride</StyledKit>
       <StyledKit>openhat</StyledKit>
       <StyledKit>boom</StyledKit>
       <StyledKit>ride</StyledKit>
@@ -18,3 +18,5 @@ export default function DrumKit() {
 
   )
 }
+
+
