d231019cf4628ce59ceffd990909b46f76386f15
sushant babu prasai <sushantprasaii7@gmail.com>
2023-10-09 20:40:44 +0545

    sound and image added

--- electronic-drum-kit/src/drum/index.jsx
+++ electronic-drum-kit/src/drum/index.jsx
@@ -1,21 +1,27 @@
+import Cymbal from "../assets/images/Cymbal"
+import Ride from "../assets/images/Ride"
+import Kicks from "../assets/images/kicks"
 import {  StyledKit, Container } from "./styling"
 
 export default function DrumKit() {
   return (
      <Container className="">
       <div className="grid grid-cols-3">
-      <StyledKit className="">crash</StyledKit>
-      <StyledKit>kick</StyledKit>
-      <StyledKit>ride</StyledKit>
+      <StyledKit>
+      <Cymbal/>
+      </StyledKit>
       <StyledKit>openhat</StyledKit>
+      <StyledKit><Ride/></StyledKit>
       <StyledKit>boom</StyledKit>
       <StyledKit>ride</StyledKit>
       <StyledKit>snare</StyledKit>
       <StyledKit>tom</StyledKit>
+      <StyledKit>
+        <Kicks/>
+      </StyledKit>
       <StyledKit>tink</StyledKit>
       </div>
       </Container> 
-
   )
 }
 
--- electronic-drum-kit/src/drum/styling.js
+++ electronic-drum-kit/src/drum/styling.js
@@ -1,14 +1,15 @@
 import styled from "styled-components";
 
 export const StyledKit = styled.div`
-  min-width: 120px;
-  height: 100px;
+  min-width: 150px;
+  height: 160px;
   border: 2px solid red;
   margin: .4rem;
   padding: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
+background:  linear-gradient(rgb(154, 125, 10), rgb(148, 49, 38));
 `;
 
 export const Container = styled.div`
